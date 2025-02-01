1d7e5803ed3b0af46d640f3e53d0ba29f85d2f35
datnq-glinteco <141558675+datnq-glinteco@users.noreply.github.com>
2025-02-01 23:24:49 +0700

    Time: 1 ms (46.28%), Space: 12.5 MB (5.85%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def isArraySpecial(self, nums):
+        for i in range(len(nums) - 1):
+                if (nums[i] % 2 == 0 and nums[i + 1] % 2 == 0) or (nums[i] % 2 == 1 and nums[i + 1] % 2 == 1):
+                    return False
+        return True
