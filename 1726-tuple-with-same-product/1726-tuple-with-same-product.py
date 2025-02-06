eb2619a0e74fb16b1f6d465bae34841f7631b6d6
datnq-glinteco <141558675+datnq-glinteco@users.noreply.github.com>
2025-02-07 00:15:52 +0700

    Time: 3507 ms (11.11%), Space: 137.5 MB (11.11%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.py
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.py
@@ -1,14 +1,25 @@
-from collections import defaultdict
-
-class Solution:
+class Solution(object):
     def tupleSameProduct(self, nums):
-        product_count = defaultdict(int)
-        ans = 0
-
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                product = nums[i] * nums[j]
-                ans += 8 * product_count[product]
-                product_count[product] += 1
-
-        return ans
+        n = len(nums)
+        
+        hash_tb = dict()
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                multi = nums[i] * nums[j]
+                if hash_tb.get(multi):
+                    hash_tb[multi].append((nums[i], nums[j]))
+                else:
+                    hash_tb[multi] = [(nums[i], nums[j])]
+        
+        number_of_pairs = 0
+        for key, value in hash_tb.items():
+            len_value = len(value)
+            if len_value >= 2:
+                number_of_pairs += self.factorial(len_value) / (self.factorial(len_value - 2) * 2)
+        return int(number_of_pairs) * 8
+    
+    def factorial(self, n):
+        res = 1
+        for i in range(1, n + 1):
+            res *= i
+        return res
