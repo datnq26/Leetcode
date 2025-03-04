78562baeae30600b3ca9f57cca9904f07abcd9a0
datnq-glinteco <141558675+datnq-glinteco@users.noreply.github.com>
2025-03-04 18:41:29 +0700

    Time: 0 ms (100%), Space: 12.4 MB (78.33%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def checkPowersOfThree(self, n):
+        while n > 0:
+            if n % 3 == 2:
+                return False
+            n = n // 3
+
+        return True
+        
