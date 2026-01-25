c4aa9eca1a601fdcc63ebe80af68eed0918679bf
Ngô Quang Đạt <141558675+datnq26@users.noreply.github.com>
2026-01-25 18:11:38 +0700

    Time: 14 ms (62.16%), Space: 44.6 MB (95.95%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MinimumDifference(int[] nums, int k) {
+        if (k == 1) return 0;
+
+        Array.Sort(nums);
+
+        int minDiff = int.MaxValue;
+
+        for (int i = 0; i <= nums.Length - k; i++)
+        {
+            int diff = nums[i + k - 1] - nums[i];
+            minDiff = Math.Min(minDiff, diff);
+        }
+
+        return minDiff;
+    }
+}
