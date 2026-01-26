720098cf91128080bc882a3f0639a25907e1236a
Ngô Quang Đạt <141558675+datnq26@users.noreply.github.com>
2026-01-26 23:17:19 +0700

    Time: 440 ms (17.43%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,1 +1,2 @@
-select e1.name as Employee from Employee as e1 Left join Employee as e2 on e1.managerId = e2.id where e1.salary > e2.salary;
+# Write your MySQL query statement below
+SELECT e.name as Employee from Employee e left JOIN Employee m on e.managerId = m.id where e.salary > m.salary and e.managerId is not null;
