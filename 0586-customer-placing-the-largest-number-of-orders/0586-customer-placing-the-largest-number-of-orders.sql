bbc23fe9dc25fd590f928f97e7f6726b5aed62d8
Ngô Quang Đạt <141558675+datnq26@users.noreply.github.com>
2026-01-27 21:40:57 +0700

    Time: 460 ms (37.55%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,1 +1,5 @@
-select customer_number from Orders group by(customer_number) order by count(*) desc limit 1;
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1;
